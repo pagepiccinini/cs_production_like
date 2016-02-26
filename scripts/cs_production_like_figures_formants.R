110e183200f265c59c02eb817cf1fc4c585a1a86
Page Piccinini <page.piccinini@gmail.com>
2016-02-26 17:59:45 +0100

    Update formants figure script.

--- scripts/cs_production_like_figures_formants.R
+++ scripts/cs_production_like_figures_formants.R
@@ -1,10 +1,22 @@
 ## READ IN DATA ####
-source("scripts/data_organization_formants_bark.R")
+source("scripts/cs_production_like_cleaning_formants.R")
 
 
 ## LOAD PACKAGES ####
 library(ggplot2)
 library(RColorBrewer)
+
+
+## ORGANIZE DATA ####
+formant_figs = formant_noout %>%
+  mutate(context_specific = factor(context_specific, levels = c("E", "CS_ES", "S", "CS_SE"),
+                                   labels=c("ML English", "CS English", "ML Spanish", "CS Spanish")))
+
+# Make data with means for percentage
+formant_sum_figs = formant_figs %>%
+  group_by(context_specific, percentage) %>%
+  summarise(f1_mean = mean(f1_norm_bark, na.rm=T), f2_mean = mean(f2_norm_bark, na.rm=T)) %>%
+  ungroup()
 
 
 ## SET COLORS ####
@@ -15,10 +27,9 @@ col_cses = cols[4]
 col_csse = cols[2]
 
 
-## PLOT FIGURE ####
-data_noout$context_specific = factor(data_noout$context_specific, levels=c("E", "CS_ES", "S", "CS_SE"), labels=c("ML English", "CS English", "ML Spanish", "CS Spanish"))
-
-formants_plot = ggplot(data_noout, aes(x=f2_norm_bark, y=f1_norm_bark, group=context_specific, linetype=context_specific)) +
+## MAKE PLOTS ####
+# Raw formants plot
+formants.plot = ggplot(formant_figs, aes(x=f2_norm_bark, y=f1_norm_bark, group=context_specific, linetype=context_specific)) +
 	geom_smooth(method = "lm", formula = y ~ x + I(x^2) + I(x^3), size = 1, color="black") +
 	ggtitle("/la\u026A/ Formant Values by\nContext and Language Preceding Token") +
 	xlab("F2 Bark Normalized") +
@@ -28,17 +39,13 @@ formants_plot = ggplot(data_noout, aes(x=f2_norm_bark, y=f1_norm_bark, group=con
 	annotate("text", x = 0.75, y = 11.9, label = "100% into /la\u026A/") +
 	theme_bw() +
 	theme(legend.title=element_blank(), text=element_text(size=18), title=element_text(size=18), legend.position=c(0.15, 0.85), panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
-	#cairo_pdf("../figures/like_formants.pdf")
-formants_plot
-	#dev.off()
+
+cairo_pdf("figures/like_formants.pdf")
+formants.plot
+dev.off()
 	
-# Make data of means
-data_means = data_noout %>%
-	group_by(context_specific, percentage) %>%
-	summarise(f1_mean = mean(f1_norm_bark, na.rm=T), f2_mean = mean(f2_norm_bark, na.rm=T)) %>%
-	ungroup()
-	
-formants_mean_plot = ggplot(data_means, aes(x=f2_mean, y=f1_mean, group=context_specific, linetype=context_specific, color=context_specific)) +
+# Mean formants plot
+formants_sum.plot = ggplot(formant_sum_figs, aes(x=f2_mean, y=f1_mean, group=context_specific, linetype=context_specific, color=context_specific)) +
 	geom_smooth(size=1) +
 	ggtitle("/la\u026A/ Formant Values by\nContext and Language Preceding Token") +
 	xlab("F2 Bark Normalized") +
@@ -53,5 +60,5 @@ formants_mean_plot = ggplot(data_means, aes(x=f2_mean, y=f1_mean, group=context_
         legend.position="top", legend.key=element_blank())
 
 cairo_pdf("figures/like_formants_means.pdf")
-formants_mean_plot
+formants_sum.plot
 dev.off()
